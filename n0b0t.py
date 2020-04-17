99366baf1526c361675d8aea14d1ace7ac55a407
Zach Lefin <zachlefin@gmail.com>
2020-04-17 17:45:01 -0400

    add load/save wallets

--- n0b0t.py
+++ n0b0t.py
@@ -1,5 +1,6 @@
+import json
 from discord import Game
-from globals import client
+from globals import client, curr_man
 import nb_commands
 
 if __name__ == '__main__':
@@ -10,5 +11,14 @@ if __name__ == '__main__':
 
     print('Starting n0b0t...')
 
+    with open('wallets.json', 'r') as f:
+        curr_man.wallets = json.loads(f.read())
+
     with open('token.txt', 'r') as f:
-        client.run(f.read())
+        try:
+            client.run(f.read())
+        finally:
+            with open('wallets.json', 'w') as f:
+                f.write(json.dumps(curr_man.wallets, indent=4))
+
+
