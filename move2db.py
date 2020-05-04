72dc0e6b764c8f0e0ef035a5c720bc8fb2f863dc
Zach Lefin <zachlefin@gmail.com>
2020-05-04 17:40:57 -0400

    init move data from json to sql db tool

---
+++ move2db.py
@@ -0,0 +1,47 @@
+import sqlite3
+import json
+from sys import argv
+
+def print_all():
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM users')
+    print(c.fetchall())
+
+
+def port():
+    with open('wallets.json', 'r') as f:
+        user_js = json.loads(f.read())
+
+    user_js = {int(k):v for k, v in user_js.items()}
+
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+
+    for k, v in user_js.items():
+        c.execute('INSERT INTO users VALUES({}, \'\', {})'.format(k, v))
+
+    conn.commit()
+
+    print_all()
+
+
+def init():
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    c.execute('CREATE TABLE users (id INT PRIMARY KEY, pwd TEXT, money INT)')
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    if len(argv) >= 2:
+        cmd = argv[1]
+        if cmd == 'init':
+            init()
+        elif cmd == 'port':
+            port()
+        elif cmd == 'print':
+            print_all()
+    else:
+        print('move2db.py init|port|print')
