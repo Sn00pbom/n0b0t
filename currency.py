9be841f70157b7b31bc96fed011dff4cef19eebf
Zach Lefin <zachlefin@gmail.com>
2020-05-05 15:11:45 -0400

    refactor do_transaction to currency

--- currency.py
+++ currency.py
@@ -9,22 +9,28 @@ class CurrencyManager(object):
     CURRENCY_SYMBOl = '₪'
     CH_ID = 699673275986608228
 
-    def __init__(self, client):
+    def __init__(self, client, userdb):
         self.client = client
         self.posts = {}
-        self.wallets = {}
+        self.userdb = userdb
 
-    def check_user(self, uid):
-        uid = str(uid)
-        if uid not in self.wallets.keys():
-            self.wallets[uid] = 0
-            return False
-        return True
+    # def check_user(self, uid):
+    #     """DEPRECATED - Use UserDB"""
+    #     return self.userdb.has_user(uid)
 
-    def user_has_value(self, uid, amount):
-        if not self.check_user(uid): return False
-        uid = str(uid)
-        return self.wallets[uid] >= amount
+    # def user_has_value(self, uid, amount):
+    #     """DEPRECATED - Use UserDB"""
+    #     # c = sqlite3.connect('users.db').cursor()
+    #     # c.execute('SELECT money FROM users WITH id={}'.format(uid))
+    #     # a = c.fetchall()
+    #     # return a[0] >= amount
+
+
+    #     if not self.check_user(uid): return False
+    #     uid = str(uid)
+    #     return self.wallets[uid] >= amount
+    def get_money(self, uid):
+        return self.userdb.get_user_value(uid, 'money')
 
     async def post_shekel(self):
         channel = self.client.get_channel(self.CH_ID)
@@ -39,9 +45,7 @@ class CurrencyManager(object):
         try:
             quantity, msg_handle = self.posts[shekel_id]
             del self.posts[shekel_id]
-            if not user_id in self.wallets.keys():
-                self.wallets[user_id] = 0
-            self.wallets[user_id] += quantity
+            self.do_transaction(uid, quantity)
             await msg_handle.delete()
             await claim_msg.delete()
             with open('money_log.txt', 'a') as f:
@@ -49,6 +53,35 @@ class CurrencyManager(object):
 
         except:
             return
+
+    async def do_transaction(self, uid, amount, succ=None, fail=None):
+        """
+        Modify a user's money value by amount (+/-)
+        Checks if user has sufficient funds if amount is negative
+
+        Parameters:
+        uid (int/str): User's id
+        amount (int): Amount to modify user's value by
+        succ (async fun): Success callback function
+        fail (async fun): Failure callback function
+
+        Returns:
+        success (bool)
+        """
+        udb = self.userdb
+        if not udb.has_user(uid):
+            if fail: await fail()
+            udb.add_user(uid)
+            return False
+
+        money = udb.get_user_value(uid, 'money')
+        if amount < 0 and money < abs(amount):  # Negative amount case with insuff funds
+            if fail: await fail()
+            return False
+        
+        udb.set_user_value(uid, 'money', money + amount)
+        if succ: await succ()
+        return True
 
 
 class MineTimer(object):
