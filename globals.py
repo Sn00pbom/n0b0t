e1737c6d278f6f7ece7e30c24a90c7a484cc9530
Zach Lefin <zachlefin@gmail.com>
2020-05-05 16:28:37 -0400

    import UserDB set global

--- globals.py
+++ globals.py
@@ -4,6 +4,7 @@ from discord.ext.commands import Bot
 
 from voting import Counsel
 from currency import CurrencyManager, MineTimer
+from data import UserDB
 
 
 PREFIX = '.'
@@ -12,6 +13,7 @@ user_contexts = {}  # user : context_id
 
 client = Bot(command_prefix=PREFIX)
 
+userdb = UserDB()
 counsel = Counsel()
-curr_man = CurrencyManager(client)
+curr_man = CurrencyManager(client, userdb)
 mine_timer = MineTimer(curr_man)
