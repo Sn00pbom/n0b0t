92f9de299117e9efef96e085c0bf52acaba7c15c
Zach Lefin <zachlefin@gmail.com>
2020-09-23 14:59:44 -0600

    add remove all dead drops on start

--- n0b0t.py
+++ n0b0t.py
@@ -12,6 +12,12 @@ if __name__ == '__main__':
     async def on_ready():
         await client.change_presence(activity=Game(CONFIG['activity']))
 
+        # Scrub dead links on start
+        channel = client.get_channel(CONFIG['tree-channel-id'])
+        async for message in channel.history(limit=1000):
+            if message.author == client.user:
+                await message.delete()
+
 
     print('Starting n0b0t...')
 
